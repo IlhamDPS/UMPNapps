d4cfa58c6ac60916527d4218f8e3ac042e48bc59
IDPS <ilhamdwi876@gmail.com>
2018-05-24 14:53:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fathurrahmanhaikal/umpnapps/about_jurusan.java
@@ -0,0 +1,13 @@
+package com.example.fathurrahmanhaikal.umpnapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class about_jurusan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_jurusan);
+    }
+}
